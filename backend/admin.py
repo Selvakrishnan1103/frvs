309904891acd9dbd6c5d6a42d168d1d5c4f70f8e
SelvaKrishnan1103 <selvakrish601@gmail.com>
2025-11-09 22:15:00 +0530

    Added dlib

--- backend/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('student_id', 'student_name', 'parent_id', 'parent_name')
--- backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('categories/', views.get_categories, name='get_categories'),
-]
-
